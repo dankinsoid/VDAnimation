408562f8ae4368268c8b75dc8a46d87766f4a3d5
Voidilov <voidilov@gmail.com>
2020-02-07 00:37:26 +0300

    no message

---
+++ VDAnimations/AnimatePropertyMapper.swift
@@ -0,0 +1,244 @@
+//
+//  Bases.swift
+//  SuperAnimations
+//
+//  Created by crypto_user on 20/09/2019.
+//  Copyright © 2019 crypto_user. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+@dynamicMemberLookup
+public struct AnimatePropertyMapper<R, T> {
+    fileprivate let object: R
+    private let keyPath: ReferenceWritableKeyPath<R, T>
+    
+    fileprivate init(object: R, keyPath: ReferenceWritableKeyPath<R, T>) {
+        self.keyPath = keyPath
+        self.object = object
+    }
+    
+    public subscript<D>(dynamicMember keyPath: WritableKeyPath<T, D>) -> AnimatePropertyMapper<R, D> {
+        let kp = self.keyPath.appending(path: keyPath)
+        return AnimatePropertyMapper<R, D>(object: object, keyPath: kp)
+    }
+    
+}
+
+private struct AnimatedPropertySetter<R, T, A: AnimationClosureProviderProtocol> {
+    fileprivate let object: R
+    private let scale: (T, Double, T) -> T
+    private let getter: () -> T?
+    private let setter: (T?) -> ()
+    
+    fileprivate init(object: R, getter: @escaping () -> T?, setter: @escaping (T?) -> (), scale: @escaping (T, Double, T) -> T) {
+        self.getter = getter
+        self.setter = setter
+        self.object = object
+        self.scale = scale
+    }
+    
+    
+    func set(_ value: T) -> AnimationProviderProtocol {
+        return _set(from: nil, value)
+    }
+    
+    func set(from initial: T, _ value: T) -> AnimationProviderProtocol {
+        return _set(from: initial, value)
+    }
+    
+     func _set(from initial: T?, _ value: T) -> AnimationProviderProtocol {
+        return PropertyAnimator(
+            from: initial,
+            getter: getter,
+            setter: setter,
+            scale: scale,
+            value: value,
+            animatorType: A.self
+        )
+    }
+    
+    func set(_ a: T, _ b: T, _ values: [T]) -> AnimationProviderProtocol {
+        set([a, b] + values)
+    }
+    
+    func set(_ values: [T]) -> AnimationProviderProtocol {
+        guard values.count > 1 else {
+            return Sequential(values.map { set($0) })
+        }
+        var array = values
+        var animations = [set(values[0])]
+        array.removeFirst()
+        animations += sequential(from: values[0], array)
+        return Sequential(animations)
+    }
+    
+    func set(from initial: T, _ a: T, _ b: T, _ values: [T]) -> AnimationProviderProtocol {
+        set(from: initial, [a, b] + values)
+    }
+    
+    func set(from initial: T, _ values: [T]) -> AnimationProviderProtocol {
+        Sequential(sequential(from: initial, values))
+    }
+    
+    private func sequential(from initial: T, _ values: [T]) -> [AnimationProviderProtocol] {
+        guard values.count > 0 else {
+            return [set(from: initial, initial)]
+        }
+        var array = values
+        var from = initial
+        var animations: [AnimationProviderProtocol] = []
+        while !array.isEmpty {
+            let second = array.removeFirst()
+            animations.append(set(from: from, second))
+            from = second
+        }
+        return animations
+    }
+    
+}
+
+extension AnimatedPropertySetter where T: Comparable {
+    
+    fileprivate func set(_ range: ClosedRange<T>) -> AnimationProviderProtocol {
+        set(from: range.lowerBound, range.upperBound)
+    }
+    
+}
+
+@dynamicMemberLookup
+public struct AnimatedPropertyMaker<R> {
+    private var object: R
+    
+    fileprivate init(object: R) {
+        self.object = object
+    }
+    
+}
+
+extension AnimatedPropertyMaker where R: UIKitPropertySettable {
+    
+    public subscript<D>(dynamicMember keyPath: ReferenceWritableKeyPath<R, D>) -> AnimatePropertyMapper<R, D> {
+        AnimatePropertyMapper(object: object, keyPath: keyPath)
+    }
+    
+}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+extension AnimatedPropertyMaker where R: View {
+    
+    public subscript<D>(dynamicMember keyPath: ReferenceWritableKeyPath<R, D>) -> AnimatePropertyMapper<R, D> {
+        AnimatePropertyMapper(object: object, keyPath: keyPath)
+    }
+    
+}
+
+extension AnimatePropertyMapper where R: UIKitPropertySettable, T: ScalableConvertable {
+    
+    private var setter: AnimatedPropertySetter<R, T, Animate> {
+        AnimatedPropertySetter(
+            object: object,
+            getter: {[weak object, keyPath] in object?[keyPath: keyPath] },
+            setter: {[weak object, keyPath] in if let v = $0 { object?[keyPath: keyPath] = v } },
+            scale: { T.init(scaleData: $0.scaleData + ($2.scaleData - $0.scaleData).scaled(by: $1)) }
+        )
+    }
+    
+    public func set(_ value: T) -> AnimationProviderProtocol { setter.set(value) }
+    public func set(from initial: T, _ value: T) -> AnimationProviderProtocol { setter.set(from: initial, value) }
+    public func set(_ a: T, _ b: T, _ values: T...) -> AnimationProviderProtocol { setter.set(a, b, values) }
+    public func set(_ values: [T]) -> AnimationProviderProtocol { setter.set(values) }
+    public func set(from initial: T, _ a: T, _ b: T, _ values: T...) -> AnimationProviderProtocol { setter.set(from: initial, a, b, values) }
+    public func set(from initial: T, _ values: [T]) -> AnimationProviderProtocol { setter.set(from: initial, values) }
+}
+
+extension AnimatePropertyMapper where R: UIKitPropertySettable, T: ScalableConvertable, T: Comparable {
+    public func set(_ range: ClosedRange<T>) -> AnimationProviderProtocol { setter.set(range) }
+}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+extension AnimatePropertyMapper where R: View, T: Animatable {
+    
+    private var setter: AnimatedPropertySetter<R, T, SwiftUIAnimate> {
+        AnimatedPropertySetter(
+            object: object,
+            getter: {[object, keyPath] in object[keyPath: keyPath] },
+            setter: {[object, keyPath] in if let v = $0 { object[keyPath: keyPath] = v } },
+            scale: {
+                var result = $0
+                var lenght = $2.animatableData - $0.animatableData
+                lenght.scale(by: $1)
+                result.animatableData += lenght
+                return result
+            }
+        )
+    }
+    
+    public func set(_ value: T) -> AnimationProviderProtocol { setter.set(value) }
+    public func set(from initial: T, _ value: T) -> AnimationProviderProtocol { setter.set(from: initial, value) }
+    public func set(_ a: T, _ b: T, _ values: T...) -> AnimationProviderProtocol { setter.set(a, b, values) }
+    public func set(_ values: [T]) -> AnimationProviderProtocol { setter.set(values) }
+    public func set(from initial: T, _ a: T, _ b: T, _ values: T...) -> AnimationProviderProtocol { setter.set(from: initial, a, b, values) }
+    public func set(from initial: T, _ values: [T]) -> AnimationProviderProtocol { setter.set(from: initial, values) }
+}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+extension AnimatePropertyMapper where R: View, T: Animatable, T: Comparable {
+    public func set(_ range: ClosedRange<T>) -> AnimationProviderProtocol { setter.set(range) }
+}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+extension AnimatePropertyMapper where R: View, T: VectorArithmetic {
+    
+    private var setter: AnimatedPropertySetter<R, T, SwiftUIAnimate> {
+        AnimatedPropertySetter(
+            object: object,
+            getter: {[object, keyPath] in object[keyPath: keyPath] },
+            setter: {[object, keyPath] in if let v = $0 { object[keyPath: keyPath] = v } },
+            scale: {
+                var lenght = $2 - $0
+                lenght.scale(by: $1)
+                return $0 + lenght
+            }
+        )
+    }
+    
+    public func set(_ value: T) -> AnimationProviderProtocol { setter.set(value) }
+    public func set(from initial: T, _ value: T) -> AnimationProviderProtocol { setter.set(from: initial, value) }
+    public func set(_ a: T, _ b: T, _ values: T...) -> AnimationProviderProtocol { setter.set(a, b, values) }
+    public func set(_ values: [T]) -> AnimationProviderProtocol { setter.set(values) }
+    public func set(from initial: T, _ a: T, _ b: T, _ values: T...) -> AnimationProviderProtocol { setter.set(from: initial, a, b, values) }
+    public func set(from initial: T, _ values: [T]) -> AnimationProviderProtocol { setter.set(from: initial, values) }
+}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+extension AnimatePropertyMapper where R: View, T: VectorArithmetic, T: Comparable {
+    public func set(_ range: ClosedRange<T>) -> AnimationProviderProtocol { setter.set(range) }
+}
+
+public protocol UIKitPropertySettable: class {}
+
+extension UIKitPropertySettable {
+    public var ca: AnimatedPropertyMaker<Self> {
+        return AnimatedPropertyMaker(object: self)
+    }
+}
+
+extension UIView: UIKitPropertySettable {}
+extension CALayer: UIKitPropertySettable {}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+extension View {
+    public var ca: AnimatedPropertyMaker<Self> {
+        return AnimatedPropertyMaker(object: self)
+    }
+}
+
+//extension NSLayoutConstraint {
+//
+//    public var ca: AnimatePropertyMapper<NSLayoutConstraint, CGFloat> {
+//        AnimatePropertyMapper(object: self, keyPath: \.constant)
+//    }
+//
+//}
--- VDAnimations/PropertyAnimator.swift
+++ VDAnimations/PropertyAnimator.swift
@@ -7,304 +7,40 @@
 //
 
 import Foundation
-import SwiftUI
 
-public protocol Scalable: AdditiveArithmetic, ScalableConvertable {
-    func scaled(by rhs: Double) -> Self
-}
-
-extension Scalable {
-    public typealias ScaleData = Self
-}
-
-public struct PropertyAnimator<T, A: AnimationClosureProviderProtocol>: AnimationProviderProtocol {
-    private let initial: () -> T?
+final class PropertyAnimator<T, A: AnimationClosureProviderProtocol>: AnimationProviderProtocol {
+    private var initial: T?
     private let value: T
     private let scale: (T, Double, T) -> T
     private let setter: (T?) -> ()
+    private let getter: () -> T?
         
-    init(from initial: @escaping () -> T?, getter: @escaping () -> T?, setter: @escaping (T?) -> (), scale: @escaping (T, Double, T) -> T, value: T, animatorType: A.Type) {
+    init(from initial: T?, getter: @escaping () -> T?, setter: @escaping (T?) -> (), scale: @escaping (T, Double, T) -> T, value: T, animatorType: A.Type) {
         self.scale = scale
         self.setter = setter
         self.initial = initial
+        self.getter = getter
         self.value = value
     }
     
-    public func start(with options: AnimationOptions, _ completion: @escaping (Bool) -> ()) {
+    func start(with options: AnimationOptions, _ completion: @escaping (Bool) -> ()) {
+        setter(initial)
+        initial = initial ?? getter()
         A.init({ self.setter(self.value) }).start(with: options, completion)
     }
     
-    public func canSet(state: AnimationState) -> Bool { true }
+    func canSet(state: AnimationState) -> Bool { true }
     
-    public func set(state: AnimationState) {
+    func set(state: AnimationState) {
+        if initial == nil { initial = getter() }
         switch state {
         case .start:
-            setter(initial())
+            setter(initial)
         case .progress(let k):
-            setter(scale(initial() ?? value, k, value))
+            setter(scale(initial ?? value, k, value))
         case .end:
             setter(value)
         }
-    }
-    
-}
-
-extension CGFloat: Scalable {
-    public func scaled(by rhs: Double) -> CGFloat { self * CGFloat(rhs) }
-}
-extension Float: Scalable {
-    public func scaled(by rhs: Double) -> Float { self * Float(rhs) }
-}
-extension Double: Scalable {
-    public func scaled(by rhs: Double) -> Double { self * rhs }
-}
-
-extension CGPoint: Scalable {
-    public func scaled(by rhs: Double) -> CGPoint { CGPoint(x: x.scaled(by: rhs), y: y.scaled(by: rhs)) }
-}
-
-extension CGSize: Scalable {
-    
-    public static func -=(lhs: inout CGSize, rhs: CGSize) {
-        lhs = lhs - rhs
-    }
-    
-    public static func -(lhs: CGSize, rhs: CGSize) -> CGSize {
-        CGSize(width: lhs.width - rhs.width, height: lhs.height - rhs.height)
-    }
-    
-    public static func += (lhs: inout CGSize, rhs: CGSize) {
-        lhs = lhs + rhs
-    }
-    
-    public static func +(lhs: CGSize, rhs: CGSize) -> CGSize {
-        CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
-    }
-    
-    public func scaled(by rhs: Double) -> CGSize {
-        CGSize(width: width.scaled(by: rhs), height: height.scaled(by: rhs))
-    }
-    
-}
-
-extension CGRect: Scalable {
-    
-    public static func -=(lhs: inout CGRect, rhs: CGRect) {
-        lhs = lhs - rhs
-    }
-    
-    public static func -(lhs: CGRect, rhs: CGRect) -> CGRect {
-        CGRect(origin: lhs.origin - rhs.origin, size: lhs.size - rhs.size)
-    }
-    
-    public static func +=(lhs: inout CGRect, rhs: CGRect) {
-        lhs = lhs + rhs
-    }
-    
-    public static func +(lhs: CGRect, rhs: CGRect) -> CGRect {
-        CGRect(origin: lhs.origin + rhs.origin, size: lhs.size + rhs.size)
-    }
-    
-    public func scaled(by rhs: Double) -> CGRect {
-        CGRect(origin: origin.scaled(by: rhs), size: size.scaled(by: rhs))
-    }
-    
-}
-
-public protocol ScalableConvertable {
-    associatedtype ScaledData: Scalable
-    var scaleData: ScaledData { get }
-    init(scaleData: ScaledData)
-}
-
-extension ScalableConvertable where Self: Scalable, ScaledData == Self {
-    public var scaleData: Self { self }
-    public init(scaleData: Self) { self = scaleData }
-}
-
-extension ScalableConvertable where Self == UIColor {
-    
-    public init(scaleData: UIColor.ScaledData) {
-        self = UIColor(red: scaleData.red.mod(1), green: scaleData.green.mod(1), blue: scaleData.blue.mod(1), alpha: scaleData.alpha.mod(1))
-    }
-    
-}
-
-fileprivate extension CGFloat {
-    
-    func mod(_ divider: CGFloat) -> CGFloat {
-        self == divider ? divider : truncatingRemainder(dividingBy: divider)
-    }
-    
-}
-
-extension UIColor: ScalableConvertable {
-    
-    public struct ScaledData: Scalable {
-        
-        public static var zero: ScaledData { ScaledData(red: 0, green: 0, blue: 0, alpha: 0) }
-        
-        public var red: CGFloat
-        public var green: CGFloat
-        public var blue: CGFloat
-        public var alpha: CGFloat
-        
-        public func scaled(by rhs: Double) -> ScaledData {
-            let rhs = CGFloat(rhs)
-            return ScaledData(red: red * rhs, green: green * rhs, blue: blue * rhs, alpha: alpha * rhs)
-        }
-        
-        public static func +(lhs: ScaleData, rhs: ScaleData) -> ScaleData {
-            ScaledData(red: lhs.red + rhs.red, green: lhs.green + rhs.green, blue: lhs.blue + rhs.blue, alpha: lhs.alpha + rhs.alpha)
-        }
-        
-        public static func +=(lhs: inout ScaledData, rhs: ScaledData) {
-            lhs = lhs + rhs
-        }
-
-        public static func -(lhs: ScaledData, rhs: ScaledData) -> ScaledData {
-            ScaledData(red: lhs.red - rhs.red, green: lhs.green - rhs.green, blue: lhs.blue - rhs.blue, alpha: lhs.alpha - rhs.alpha)
-        }
-        
-        public static func -=(lhs: inout ScaledData, rhs: ScaledData) {
-            lhs = lhs - rhs
-        }
-        
-    }
-    
-    public var scaleData: ScaledData {
-        var r: CGFloat = 0
-        var g: CGFloat = 0
-        var b: CGFloat = 0
-        var a: CGFloat = 0
-        if getRed(&r, green: &g, blue: &b, alpha: &a) {
-            return ScaledData(red: r, green: g, blue: b, alpha: a)
-        }
-        return .zero
-    }
-    
-}
-
-extension CGAffineTransform: Scalable {
-    
-    public static var zero: CGAffineTransform { .identity }
-    
-    public func scaled(by rhs: Double) -> CGAffineTransform {
-        let k = CGFloat(rhs)
-        return CGAffineTransform(a: a * k, b: b * k, c: c * k, d: d * k, tx: tx * k, ty: ty * k)
-    }
-    
-    public static func +(lhs: CGAffineTransform, rhs: CGAffineTransform) -> CGAffineTransform {
-        CGAffineTransform(a: (lhs.a + rhs.a) / 1, b: (lhs.b + rhs.b) / 1, c: (lhs.c + rhs.c) / 1, d: (lhs.d + rhs.d) / 1, tx: (lhs.tx + rhs.tx) / 1, ty: (lhs.ty + rhs.ty) / 1)
-    }
-    
-    public static func +=(lhs: inout CGAffineTransform, rhs: CGAffineTransform) {
-        lhs = lhs + rhs
-    }
-    
-    public static func -(lhs: CGAffineTransform, rhs: CGAffineTransform) -> CGAffineTransform {
-        lhs + CGAffineTransform(a: -rhs.a, b: -rhs.b, c: -rhs.c, d: -rhs.d, tx: -rhs.tx, ty: -rhs.ty)
-    }
-    
-    public static func -=(lhs: inout CGAffineTransform, rhs: CGAffineTransform) {
-        lhs = lhs - rhs
-    }
-    
-}
-
-extension CATransform3D: Scalable {
-    public static var zero: CATransform3D { CATransform3DIdentity }
-    public struct Row: Equatable {
-        public let (x1, x2, x3, x4): (CGFloat, CGFloat, CGFloat, CGFloat)
-    }
-    public struct Matrix: Equatable {
-        public let (x1, x2, x3, x4): (Row, Row, Row, Row)
-        public init(_ x1: (CGFloat, CGFloat, CGFloat, CGFloat), _ x2: (CGFloat, CGFloat, CGFloat, CGFloat), _ x3: (CGFloat, CGFloat, CGFloat, CGFloat), _ x4: (CGFloat, CGFloat, CGFloat, CGFloat)) {
-            self.x1 = Row(x1: x1.0, x2: x1.1, x3: x1.2, x4: x1.3)
-            self.x2 = Row(x1: x2.0, x2: x2.1, x3: x2.2, x4: x2.3)
-            self.x3 = Row(x1: x3.0, x2: x3.1, x3: x3.2, x4: x3.3)
-            self.x4 = Row(x1: x4.0, x2: x4.1, x3: x4.2, x4: x4.3)
-        }
-    }
-    
-    public var matrix: Matrix { Matrix(
-        (m11, m12, m13, m14),
-        (m21, m22, m23, m24),
-        (m31, m32, m33, m34),
-        (m41, m42, m43, m44)
-        )
-    }
-    
-    public init(_ m: Matrix) {
-        self = CATransform3D(
-            m11: m.x1.x1, m12: m.x1.x2, m13: m.x1.x3, m14: m.x1.x4,
-            m21: m.x2.x1, m22: m.x2.x2, m23: m.x2.x3, m24: m.x2.x4,
-            m31: m.x3.x1, m32: m.x3.x2, m33: m.x3.x3, m34: m.x3.x4,
-            m41: m.x4.x1, m42: m.x4.x2, m43: m.x4.x3, m44: m.x4.x4
-        )
-    }
-    
-    public func scaled(by rhs: Double) -> CATransform3D {
-        let k = CGFloat(rhs)
-        return CATransform3D(
-            m11: k * m11, m12: k * m12, m13: k * m13, m14: k * m14,
-            m21: k * m21, m22: k * m22, m23: k * m23, m24: k * m24,
-            m31: k * m31, m32: k * m32, m33: k * m33, m34: k * m34,
-            m41: k * m41, m42: k * m42, m43: k * m43, m44: k * m44
-        )
-    }
-    
-    public static func ==(lhs: CATransform3D, rhs: CATransform3D) -> Bool {
-        lhs.matrix == rhs.matrix
-    }
-    
-    public static func +(lhs: CATransform3D, rhs: CATransform3D) -> CATransform3D {
-        operation(lhs: lhs, rhs: rhs, o: +)
-    }
-    
-    public static func +=(lhs: inout CATransform3D, rhs: CATransform3D) {
-        lhs = lhs + rhs
-    }
-    
-    public static func -(lhs: CATransform3D, rhs: CATransform3D) -> CATransform3D {
-        operation(lhs: lhs, rhs: rhs, o: -)
-    }
-    
-    public static func -=(lhs: inout CATransform3D, rhs: CATransform3D) {
-        lhs = lhs - rhs
-    }
-    
-    private static func operation(lhs: CATransform3D, rhs: CATransform3D, o: @escaping (CGFloat, CGFloat) -> CGFloat) -> CATransform3D {
-        let op: (KeyPath<CATransform3D, CGFloat>) -> CGFloat = {
-            o(lhs[keyPath: $0], rhs[keyPath: $0])
-        }
-        return CATransform3D(
-            m11: op(\.m11), m12: op(\.m12), m13: op(\.m13), m14: op(\.m14),
-            m21: op(\.m21), m22: op(\.m22), m23: op(\.m23), m24: op(\.m24),
-            m31: op(\.m31), m32: op(\.m32), m33: op(\.m33), m34: op(\.m34),
-            m41: op(\.m41), m42: op(\.m42), m43: op(\.m43), m44: op(\.m44)
-        )
-    }
-    
-}
-
-extension Optional: ScalableConvertable where Wrapped: ScalableConvertable {
-    public typealias ScaledData = Wrapped.ScaledData
-    public var scaleData: Wrapped.ScaledData { self?.scaleData ?? .zero }
-    
-    public init(scaleData: Wrapped.ScaledData) {
-        self = .some(Wrapped.init(scaleData: scaleData))
-    }
-    
-}
-
-@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
-extension Optional: Animatable where Wrapped: Animatable {
-    public typealias AnimatableData = Wrapped.AnimatableData
-    
-    public var animatableData: Wrapped.AnimatableData {
-        get { self?.animatableData ?? .zero }
-        set { self?.animatableData = newValue }
     }
     
 }
