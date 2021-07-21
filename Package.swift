ae2a060fa520c1079ba5e64b69b6bee6d8759276
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2021-07-22 00:55:52 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -13,19 +13,18 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "VDAnimation",
-            targets: ["VDAnimation"]),
+            targets: ["VDAnimation"]
+				),
     ],
     dependencies: [
-        .package(url: "https://github.com/dankinsoid/ConstraintsOperators.git", from: "2.3.4")
+        .package(url: "https://github.com/dankinsoid/ConstraintsOperators.git", from: "2.24.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "VDAnimation",
-            dependencies: ["ConstraintsOperators"]),
-        .testTarget(
-            name: "VDAnimationTests",
-            dependencies: ["VDAnimation"]),
+            dependencies: ["ConstraintsOperators"]
+				)
     ]
 )
